ed5c409646aecab10d56b0a8cbc7c3cd1a075f30
Hercule0 <111614460+Hercule0@users.noreply.github.com>
2026-09-04 15:48:30 +0300

    Fix408: block extra terminals without multi entitlement

--- public/api/v2/activate.php
+++ public/api/v2/activate.php
@@ -1,8 +1,13 @@
 <?php
 require_once __DIR__ . '/_common.php';
+require_once __DIR__ . '/../../../includes/MultiEntitlementPolicy.php';
 $input = v2_input();
 v2_rate_limit('activate', $input);
 try {
+    $policy = MultiEntitlementPolicy::preflightActivation($input);
+    if (!($policy['ok'] ?? false)) {
+        v2_signed_response($policy);
+    }
     v2_signed_response(EntitlementV2::activate($input, client_ip()));
 } catch (Throwable $e) {
     v2_exception_response($e);
